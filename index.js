d6ecc3f3b1edaca1e5a28516dbbdb6f429842c31
Felipe Delgado <af.delgador@uniandes.edu.co>
2021-06-29 16:55:33 -0500

    increment button

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+let count = 5
+
+count = count + 1
+// let age = 22
+
+// console.log(age)
+
+console.log(count)
+
+// let age = 22
+// let ratio = 7
+// let dogAge = age * ratio
+// console.log(dogAge)
