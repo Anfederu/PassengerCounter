9d64983f2c64e155c132f6a78f27f31872efd17c
Felipe Delgado <af.delgador@uniandes.edu.co>
2021-06-29 17:11:37 -0500

    boton funcionando

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 //document.getElementById("count-el").innerText = 5
 
-let count = 5
+// let count = 5
 
-count = count + 1
-// let age = 22
+// count = count + 1
+// // let age = 22
 
-// console.log(age)
+// // console.log(age)
 
-console.log(count)
+// console.log(count)
 
 // let age = 22
 // let ratio = 7
 // let dogAge = age * ratio
 // console.log(dogAge)
+let count = 0
+
+function increment() {
+    count = count + 1
+    ocument.getElementById("count-el").innerText = count
+}
